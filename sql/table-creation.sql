24012f127359c6b8d4c82eb74142bc8d280782b2
Vero Castillo <77795761+verocastillo@users.noreply.github.com>
2021-06-10 20:11:33 -0500

    Added table creation sql file.

---
+++ sql/table-creation.sql
@@ -0,0 +1,64 @@
+-- ETL Project: Happiness Database
+-- Table Creation
+
+-- 1. Drop tables if they already exist
+DROP TABLE IF EXISTS countries;
+DROP TABLE IF EXISTS regions;
+DROP TABLE IF EXISTS happiness;
+DROP TABLE IF EXISTS health;
+DROP TABLE IF EXISTS sustainability;
+DROP TABLE IF EXISTS economy;
+
+--2.Create tables and set constraints
+CREATE TABLE "countries" (
+    "country_id" INT   NOT NULL,
+    "country_name" VARCHAR(50)   NOT NULL,
+    "population" FLOAT   NOT NULL,
+    "region_id" INT   NOT NULL,
+    CONSTRAINT "pk_countries" PRIMARY KEY (
+        "country_id"
+     )
+);
+
+CREATE TABLE "regions" (
+    "region_id" INT   NOT NULL,
+    "region_name" VARCHAR(50)   NOT NULL,
+    CONSTRAINT "pk_regions" PRIMARY KEY (
+        "region_id"
+     )
+);
+
+CREATE TABLE "happiness" (
+    "country_id" INT   NOT NULL,
+    "happiness_index" FLOAT   NOT NULL,
+    "social_support" FLOAT   NOT NULL,
+    "freedom" FLOAT   NOT NULL
+);
+
+CREATE TABLE "health" (
+    "country_id" INT   NOT NULL,
+    "healthy_life_exp" FLOAT   NOT NULL
+);
+
+CREATE TABLE "sustainability" (
+    "country_id" INT   NOT NULL,
+    "renewable_energy" FLOAT   NOT NULL,
+    "access_electricity" FLOAT   NOT NULL,
+    "access_water" FLOAT   NOT NULL
+);
+
+CREATE TABLE "economy" (
+    "country_id" INT   NOT NULL,
+    "gdp_per_capita" FLOAT   NOT NULL,
+    "ppp" FLOAT   NOT NULL,
+    "unemployment" FLOAT   NOT NULL,
+    "income_per_capita" FLOAT   NOT NULL
+);
+
+--3.Check if tables were created
+SELECT * FROM countries;
+SELECT * FROM regions;
+SELECT * FROM happiness;
+SELECT * FROM health;
+SELECT * FROM sustainability;
+SELECT * FROM economy;
